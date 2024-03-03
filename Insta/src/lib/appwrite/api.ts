279acecd20fab0c9fe59e39f182b55c09c7fae6a
kanishkazoysa <125685188+kanishkazoysa@users.noreply.github.com>
2024-03-03 18:35:31 +0530

    Insta | edit

--- Insta/src/lib/appwrite/api.ts
+++ Insta/src/lib/appwrite/api.ts
@@ -1,20 +1,53 @@
 import { INewUser } from "@/types";
-import { ID } from 'appwrite';
-import { account } from './config';
+import { ID } from "appwrite";
+import { account, appwriteConfig, avatars,  databases } from "./config";
 
+export async function createUserAccount(user: INewUser) {
+  try {
+    const newAccount = await account.create(
+      ID.unique(),
+      user.email,
+      user.password,
+      user.name
+    );
 
-export async function createUserAccount(user:INewUser) {
-    try{
-        const newAccount = await account.create(
-                ID.unique(),
-                user.email,
-                user.password,
-                user.name
-                );
-                return newAccount
-        
-    }catch(error){
-        console.error(error);
-        return error;
+    if (!newAccount) {
+      throw Error;
     }
+    const avatarUrl =avatars.getInitials(user.name);
+    
+    const newUser = await saveUserToDB({
+        accountId: newAccount.$id,
+        name: newAccount.name,
+        email: newAccount.email,
+        username: user.username,
+        imageUrl: avatarUrl,
+      });
+
+    return newUser;
+  } catch (error) {
+    console.error(error);
+    return error;
+  }
 }
+
+export async function saveUserToDB(user: {
+    accountId: string;
+    email: string;
+    name: string;
+    imageUrl: URL;
+    username?: string;
+  }) {
+    try {
+      const newUser = await databases.createDocument(
+        appwriteConfig.databaseId,
+        appwriteConfig.userCollectionId,
+        ID.unique(),
+        user
+      );
+  
+      return newUser;
+    } catch (error) {
+      console.log(error);
+    }
+  }
